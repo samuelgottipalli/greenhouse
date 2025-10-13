c7d53b3d919178ebdd36a57cbdeb5b9b1d3cce0a
sgottipalli-unr <sgottipalli@unr.edu>
2025-10-13 16:49:20 -0700

    Enhance app settings UI by adding date and time format options; update weather data display logic and improve session state management

--- server-streamlit/appsettings.py
+++ server-streamlit/appsettings.py
@@ -11,5 +11,9 @@ st.title("App Settings")
 with st.container(border=True):
     units = st.radio("Display Units", ["SI", "US"], horizontal=True)
     st.session_state["units"] = units
-    theme = st.radio("Display Theme", ["Light", "Dark", "System"], horizontal=True)
-    st.session_state["theme"] = theme
+    # theme = st.radio("Display Theme", ["Light", "Dark", "System"], horizontal=True)
+    # st.session_state["theme"] = theme
+    date_format = st.radio("Date Format", ["DD/MM/YYYY", "MM/DD/YYYY", "YYYY/MM/DD", "Day, Month DD, YYYY"], horizontal=True)
+    st.session_state["date_format"] = date_format
+    time_format = st.radio("Time Format", ["12-hour", "24-hour"], horizontal=True)
+    st.session_state["time_format"] = time_format
--- server-streamlit/weather.py
+++ server-streamlit/weather.py
@@ -1,8 +1,9 @@
 import time
+from turtle import up
 from pandas import DataFrame
 import streamlit as st
 from config import weather_code_descr
-from datetime import timedelta
+from datetime import timedelta, datetime as dtt
 
 
 st.set_page_config(
@@ -10,10 +11,17 @@ st.set_page_config(
     page_title="Greenhouse Control Center",
     page_icon="🌦️",
     # Make the content take up the width of the page:
-    layout="centered",
+    layout="wide",
 )
 
 st.title("Weather Data")
+if not "units" in st.session_state:
+    st.session_state["units"] = "SI"
+if st.session_state["units"] == "US":
+    unit = "F"
+else:
+    unit = "C"
+
 
 @st.fragment(run_every=timedelta(minutes=15))
 def get_weather_data():
@@ -38,6 +46,34 @@ def get_weather_data():
         weathertoast.toast(f"Unable to fetch weather data from DB. Possibly due to a database error. Error: {e}", icon=":material/error:")
         return None
 
+@st.fragment(run_every=timedelta(minutes=1))
+def update_datetime():
+    """
+    Update the time on the page.
+    """
+
+    if "date_format" not in st.session_state:
+        st.session_state["date_format"] = "MM/DD/YYYY"
+    if st.session_state["date_format"] == "DD/MM/YYYY":
+        display_date = dtt.now().strftime("%d/%m/%Y")
+    elif st.session_state["date_format"] == "MM/DD/YYYY":
+        display_date = dtt.now().strftime("%m/%d/%Y")
+    elif st.session_state["date_format"] == "YYYY/MM/DD":
+        display_date = dtt.now().strftime("%Y/%m/%d")
+    else:
+        display_date = dtt.now().strftime("%A, %d %B %Y")
+
+    if "time_format" not in st.session_state:
+        st.session_state["time_format"] = "12h"
+    if st.session_state["time_format"] == "12h":
+        display_time = dtt.now().strftime("%I:%M %p")
+    else:
+        display_time = dtt.now().strftime("%H:%M")
+    with st.container(border=True, horizontal=True):
+        st.metric(label="Date", value=display_date)
+        st.metric(label="Time", value=display_time)
+
+update_datetime()
 # Create a text element and let the reader know the data is loading.
 weathertoast = st.toast("Fetching weather data...")
 data = get_weather_data()
@@ -46,15 +82,25 @@ data = get_weather_data()
 if data is not None:
     metrics_container = st.container(border=True,horizontal=True)
     with metrics_container:
-        st.metric(label="Temperature", value=data.iloc[-1]["TEMPERATURE"])
-        st.metric(label="Humidity", value=data.iloc[-1]["RELATIVE_HUMIDITY"])
-        st.metric(label="Precipitation", value=data.iloc[-1]["PRECIPITATION"])
+        # if st.session_state["units"] == "US":
+
+        st.metric(label="Temperature", value=data.iloc[-1]["TEMPERATURE"], border=True)
+        st.metric(
+            label="Humidity", value=data.iloc[-1]["RELATIVE_HUMIDITY"], border=True
+        )
+        st.metric(
+            label="Precipitation", value=data.iloc[-1]["PRECIPITATION"], border=True
+        )
         weathercode = data.iloc[-1]["WEATHER_CODE"]
         if weathercode in weather_code_descr:
-            st.metric(label="Weather", value=weather_code_descr[weathercode])
+            st.metric(
+                label="Weather",
+                value=weather_code_descr[weathercode],
+                border=True,
+            )
         else:
-            st.metric(label="Weather", value="Unknown")
-        st.metric(label="Wind Speed", value=data.iloc[-1]["WIND_SPEED"])
+            st.metric(label="Weather", value="Unknown", border=True)
+        st.metric(label="Wind Speed", value=data.iloc[-1]["WIND_SPEED"], border=True)
     if st.checkbox('Show raw data'):
         st.subheader('Raw data')
         st.write(data)
